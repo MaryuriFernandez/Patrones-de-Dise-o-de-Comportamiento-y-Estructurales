6deb646ca8209217f68a105ed4ea842548be82c7
MaryuriFernandez <maryurifernandez437@gmail.com>
2025-10-07 13:31:38 -0500

    🩹cambio en la forma de evaluacion y asignacion de evaluadores

--- src/main/java/co/edu/unicauca/DesignPatterns/domain/facade/EvaluationSystem.java
+++ src/main/java/co/edu/unicauca/DesignPatterns/domain/facade/EvaluationSystem.java
@@ -3,16 +3,10 @@ import co.edu.unicauca.DesignPatterns.domain.entities.Project;
 
 public class EvaluationSystem {
     public boolean evaluate(Project project) {
-        System.out.println("---------------------------------------------------------------------");
         System.out.println("Evaluando proyecto: " + project.getTitulo());
         System.out.println("del estudiante: " + project.getEstudiante1().getFullName());
 
-        boolean approved = Math.random() > 0.2;
-        if (approved) {
-            System.out.println("Proyecto aprovado");
-        } else {
-            System.out.println("Proyecto no aprovado.");
-        }
+        boolean approved = Math.random() > 0.3;
         System.out.println("---------------------------------------------------------------------");
         return approved;
     }
--- src/main/java/co/edu/unicauca/DesignPatterns/domain/facade/EvaluatorsAssignment.java
+++ src/main/java/co/edu/unicauca/DesignPatterns/domain/facade/EvaluatorsAssignment.java
@@ -3,14 +3,8 @@ import co.edu.unicauca.DesignPatterns.domain.entities.Project;
 
 public class EvaluatorsAssignment {
     public boolean assign(Project project) {
-        System.out.println("---------------------------------------------------------------------");
         System.out.println("Asignando evaluadores para el proyecto: " + project.getTitulo());
-        boolean assigned = Math.random() > 0.3;
-        if (assigned) {
-            System.out.println("Evaluadores asignados exitosamente.");
-        } else {
-            System.out.println("No hay evaluadores disponibles para este proyecto.");
-        }
+        boolean assigned = Math.random() > 0.4;
         System.out.println("---------------------------------------------------------------------");
         return assigned;
     }
